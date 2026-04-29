e92d5fa43c3dee23ca576a240ce8b9dfaa315e9e
Kinga Żmuda <kinga.zmuda@icloud.com>
2026-04-29 08:13:19 +0200

    adjust experience card styling

--- components/reusable/ExperienceCard.tsx
+++ components/reusable/ExperienceCard.tsx
@@ -3,12 +3,12 @@ import { ExperienceEntry } from "@/types";
 export default function ExperienceCard({ position, company, start, end, description }: ExperienceEntry) {
     return (
         <div className="mb-6 rounded-lg bg-[#1A2035] p-4 transition-transform hover:scale-102">
-            <div className="mb-2 flex items-center justify-between">
+            <div className="mb-1 flex items-center justify-between">
                 <h3 className="text-lg font-bold">{position}</h3>
                 <p className="text-slate-500">{start} - {end}</p>
             </div>
-            <p className="text-slate-400">{company}</p>
-            <p className="text-slate-400">{description}</p>
+            <p className="text-slate-400 font-bold mb-1">{company}</p>
+            <p className="text-sm text-slate-400">{description}</p>
         </div>  
     );
 }
